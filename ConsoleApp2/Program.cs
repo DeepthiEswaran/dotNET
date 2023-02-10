ddded9f6b7df4d93087895dcf2966beb30b983bb
DeepthiEswaran <121942956+DeepthiEswaran@users.noreply.github.com>
2023-02-10 15:56:53 +0530

    ConsoleApp

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SampleLib;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MathComponent.Add(23, 23));
+        }
+    }
+}
